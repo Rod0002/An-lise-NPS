dc45fddc2d38b5993e2bd925aeca91278319b75a
Rod0002 <101226338+Rod0002@users.noreply.github.com>
2025-05-19 12:19:13 -0300

    Delete Downloads directory

--- Downloads/hospitaisfeedback(1)/hospitaisfeedback/src/main/java/com/uninove/hospitaisfeedback/Repository/HospitalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.uninove.hospitaisfeedback.Repository;
-
-import com.uninove.hospitaisfeedback.Model.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HospitalRepository extends JpaRepository<Hospital, Long> {
-}
